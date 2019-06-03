e0e25f329b3e754b834ef0591708cece966b7f65
Ash <ash@gscloudsolutions.com>
2019-06-02 21:05:44 -0700

    Added ConvertImageUrlToBase64()

--- main.go
+++ main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
+func HandleHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("HandleHome"))
+}
+
+func HandleFormatImage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("HandleFormatImage"))
+}
+
+func ConvertImageUrlToBase64(url string) string {
+	var client http.Client
+	response, err := client.Get(url)
+	HandleError(err)
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	HandleError(err)
+	// bodyString := string(bodyBytes)
+
+	return base64.StdEncoding.EncodeToString(bodyBytes)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	url := "https://cdn-images-1.medium.com/max/800/1*30aoNxlSnaYrLhBT0O1lzw.png"
+	fmt.Println(ConvertImageUrlToBase64(url))
+	// resp, _ := http.Get(url)
+	// fmt.Println(resp.Body)
+	// file, _ := os.Create("testfile")
+	// io.Copy(file, resp.Body)
+
+	// router := mux.NewRouter()
+	// router.HandleFunc("/", HandleHome)
+	// router.HandleFunc("/api/format-image", HandleFormatImage)
+
+	// http.ListenAndServe(":8080", router)
+}
+
+func HandleError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }

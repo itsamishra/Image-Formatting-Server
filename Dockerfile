95c60bae635c8ff656775adb8dde25453fa48430
Ash <ash@gscloudsolutions.com>
2019-06-17 20:50:59 -0700

    Added docker integration

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /usr/src/app
 
 COPY ./src .
 
+EXPOSE 5000
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 CMD [ "python", "./main.py" ]
--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
-from flask import app
+from flask import Flask
 
-print(app)
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'hello world!!!'
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')

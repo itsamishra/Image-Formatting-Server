2e7839a2f6e6720dfa0805ecd05c63a98c5cc38f
Ash <ash@gscloudsolutions.com>
2019-06-19 02:10:27 -0700

    Added requests library

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'hello world!!!'
 
-if __name__=='__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+def get_image(image_link):
+    response = requests.get(image_link, stream=True)
+    print(response)
+    print('--------------------------------------------------------------------')
+    print(response.raw)
+    print('--------------------------------------------------------------------')
+    print(response.raw.data)
+
+
+@app.route("/")
+def handle_home():
+    return "Go to: /api/format-image"
+
+
+@app.route("/api/format-image")
+def handle_format_image():
+    get_image(
+        "https://www.python.org/static/community_logos/python-logo-master-v3-TM.png"
+    )
+
+    return "Format Image"
+
+
+# Runs server
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
